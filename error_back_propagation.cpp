9da8f95675f698e85446d9794812e7bf613b95ec
sieun <tlatldms@naver.com>
2019-12-13 20:06:25 +0900

    calculated sum

--- error_back_propagation.cpp
+++ error_back_propagation.cpp
@@ -11,7 +11,8 @@ vector<double> target = { 0,1,1,0 };
 vector<double> weight1 = { -0.089, 0.028, 0.092 };
 vector<double> weight2 = { 0.098, -0.07, -0.01 };
 vector<double> v = {0.056, 0.067, 0.016};
-vector<double> dv(3), dw1(3), dw2(3);
+vector<double> vsum(3), w1sum(3), w2sum(3);
+vector<vector<double>> dv(4, vector<double> (3)), dw1(4, vector<double>(3)), dw2(4, vector<double>(3));
 
 //int i: 몇번째 케이스인지
 void fun(int idx) {
@@ -20,7 +21,9 @@ void fun(int idx) {
 		+ 1 * weight1[2];
 	double net1_2 = input1[idx] * weight2[0] + input2[idx] * weight2[1]
 		+ 1 * weight2[2];
+
 	vector<double> h(3);
+	
 	h[0] = 1 / (1 + exp(-net1_1));
 	h[1] = 1 / (1 + exp(-net1_2));
 	h[2] = 1;
@@ -32,20 +35,35 @@ void fun(int idx) {
 	printf("net1_1: %f, net1_2: %f, h1: %f, h2: %f, net2: %f, output: %f\n"
 		, net1_1, net1_2, h[0], h[1], net2, output);
 	
+	cout << "now idx(input case): " << idx+1 << endl;
 	for (int i = 0; i < 3; i++) {
 	
+		printf("i: %d\n", i);
+		dw1[idx][i] = -input1[idx] * h[i] * (1 - h[i]) * (v[i] * (target[idx] - output) * output * (1 - output));
+		dw2[idx][i] = -input2[idx] * h[i] * (1 - h[i]) * (v[i] * (target[idx] - output) * output * (1 - output));
+		printf("dw1: %f, dw2: %f\n", dw1[idx][i], dw2[idx][i]);
 
-		//dw1[i] = -input1[idx]*h[i]*(1-h[i])*
-		
-		dv[i] = -(target[idx] - output) * output * (1 - output) * h[i];
-		printf("i: %d, dv: %f\n", i, dv[i]);
-
+		dv[idx][i] = -(target[idx] - output) * output * (1 - output) * h[i];
+		printf("dv: %f\n", dv[idx][i]);
 	}
+
+
 }
 
 int main() {	
-	
-	fun(0);
+	for (int i = 0; i < 4; i++) {
+		fun(i);
+		cout << "\n\n\n";
+	}
+	for (int i = 0; i < 3; i++) { //i: v number
+		for (int j = 0; j < 4; j++) { //j: case number
+			vsum[i] += dv[j][i];
+			w1sum[i] += dw1[j][i];
+			w2sum[i] += dw2[j][i];
+		}
 
+		printf("i: %d, vsum: %f, w1sum: %f, w2sum: %f \n", i, vsum[i], w1sum[i], w2sum[i]);
+	}
+	
 	return 0;
 }
